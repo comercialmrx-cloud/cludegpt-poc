a8700ad7b4af9714db04e9182d4d42f169b60035
comercialmrx-cloud <comercial.mrx@gmail.com>
2026-06-18 12:59:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 
